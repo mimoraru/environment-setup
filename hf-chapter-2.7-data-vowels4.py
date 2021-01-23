22e3b3ebda7c3b8378850e3ba37c876f715999c7
Mihai Moraru <moraru.mihai@hotmail.com>
2021-01-23 12:26:15 +0100

    covered dictionaries

--- hf-chapter-2.7-data-vowels4.py
+++ hf-chapter-2.7-data-vowels4.py
@@ -5,7 +5,7 @@ found = {}
 
 for letter in word:
     if letter in vow:
-        found.setdefault(letter, 0)
+        found.setdefault(letter, 0)ana
         found[letter] += 1
 
 for k, v in sorted(found.items()):
